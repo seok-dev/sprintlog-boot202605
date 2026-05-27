1aca4bd78bcbd825486457d4ec4e3c0c4a5ed62c
seok-dev <kseok.dev@gmail.com>
2026-05-27 09:41:46 +0900

    Initial commit

---
+++ src/main/java/com/sprintlog/sprintlogboot/SprintlogBootApplication.java
@@ -0,0 +1,13 @@
+package com.sprintlog.sprintlogboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintlogBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintlogBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprintlog/sprintlogboot/SprintlogBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintlog.sprintlogboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintlogBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
